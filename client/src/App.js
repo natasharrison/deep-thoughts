dc5cdb5fc605fdbae05b0fd648e9bbba0cd41e84
natasharrison <natasharrison@gmail.com>
2021-08-03 11:07:12 -0700

    authentication

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,15 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
   uri: '/graphql'
 });
 
@@ -42,4 +51,3 @@ function App() {
 }
 
 export default App;
-
