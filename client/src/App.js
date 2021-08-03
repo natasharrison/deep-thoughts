736e089c319de2be60dd4e524bce6a2740fc5888
natasharrison <natasharrison@gmail.com>
2021-08-03 09:20:31 -0700

    apollo bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
