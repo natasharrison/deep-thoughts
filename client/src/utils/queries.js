22f3278026ada3acba6ad4e7ce107c3c93ec8dee
natasharrison <natasharrison@gmail.com>
2021-08-03 09:01:21 -0700

    feature/routes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,4 @@
-// this file will store all of the GraphQL query requests
-
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag';
 
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
@@ -15,6 +13,45 @@ export const QUERY_THOUGHTS = gql`
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
